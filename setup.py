1cdadf0bbbf5f61a7f44e5def4b2359b9e7bd375
Maksim Ekimovskii <me@multichannel.net>
2014-02-04 21:17:03 +0800

    Addded docs and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='simple-models',
+    version='0.1.0',
+    packages=['core'],
+    url='',
+    license='GPL v3',
+    author='maxi',
+    author_email='me@multichannel.net',
+    description='Dict-like simple models for structure your any internal models. Useful for API or for any data which prefer to be structured '
+)
