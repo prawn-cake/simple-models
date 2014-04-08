70007721d9d0f88c21a00ad010c411e23d3a1312
Maksim Ekimovskii <me@multichannel.net>
2014-04-08 19:37:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='simple-models',
-    version='0.1.0',
+    version='0.1.1',
     packages=['simplemodels'],
     url='https://github.com/prawn-cake/simple-models',
     license='MIT',
