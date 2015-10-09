eec26abc39ff0b5f1c025bdd56f2f42d9d16355d
Maksim Ekimovskii <ekimovsky.maksim@gmail.com>
2015-10-10 01:21:54 +0800

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='simple-models',
-    version='0.3.0',
+    version='0.3.1',
     packages=['simplemodels'],
     url='https://github.com/prawn-cake/simple-models',
     license='MIT',
--- simplemodels/__init__.py
+++ simplemodels/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 import sys
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 PYTHON_VERSION = sys.version_info[0]
