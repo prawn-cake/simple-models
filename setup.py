18df6838a38afd95cf6d7e65c9883da428abb51e
Maksim Ekimovskii <ekimovsky.maksim@gmail.com>
2016-10-02 10:17:52 +0300

    fix install_requires option

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='simple-models',
     version='0.5.1',
     packages=['simplemodels'],
-    install_required=['six==1.9.0'],
+    install_requires=['six==1.9.0'],
     url='https://github.com/prawn-cake/simple-models',
     license='MIT',
     author='Maksim Ekimovskii',
