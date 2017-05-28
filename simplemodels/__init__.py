a6f70bf0105d17b830f52f66c2701c01d7e6091a
Maksim Ekimovskii <ekimovsky.maksim@gmail.com>
2017-05-28 15:24:52 +0300

    Added public imports interface for .models and .fields module

--- simplemodels/__init__.py
+++ simplemodels/__init__.py
@@ -3,3 +3,7 @@ import sys
 
 __version__ = '0.5.2'
 PYTHON_VERSION = sys.version_info[0]
+
+# Public imports interface
+from .models import Document, ImmutableDocument
+from .fields import *  # __all__ listed fields should be imported here
